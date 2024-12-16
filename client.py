0dd7ad2274cfcb9a246606f9770cd45f98ba6074
Vinogradov Konstantin <145968889+MiRRoRise@users.noreply.github.com>
2024-12-16 11:33:23 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
 import socket
-
-def run_client(host="127.0.0.1", port=65432):
+import sys
+def run_client(host="127.0.0.1", port=65433):
     # Создаем клиентский сокет
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((host, port))
+        client_socket.settimeout(10)
+        try:
+            client_socket.connect((host, port))
+        except TimeoutError:
+            print("\nСервер не отвечает. Клиент завершает работу.")
+            return
+        except ConnectionResetError:
+            print("\nСервер не отвечает. Клиент завершает работу.")
+            return
         print(f"Соединение с сервером {host}:{port} установлено.")
-
+        
         try:
             while True:
                 # Отправляем сообщение "ping"
@@ -18,9 +26,14 @@ def run_client(host="127.0.0.1", port=65432):
 
                 # Получаем ответ от сервера
                 data = client_socket.recv(1024)
+                if(data.decode()==""):
+                    print("Сервер завершает свою работу. Завершаем работу клиента.")
+                    break
                 print(f"Ответ сервера: {data.decode()}")
         except KeyboardInterrupt:
             print("\nКлиент завершает работу.")
 
+
 if __name__ == "__main__":
-    run_client()
+    run_client(sys.argv[1],int(sys.argv[2]))
+
